3590dea9c850d005ded04f0141c70b7504b737aa
michael09232002 <102804007+michael09232002@users.noreply.github.com>
2022-04-02 06:09:12 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hardware Heroes</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+    <div class="container">
+    <div class="navbar">
+        <img src="Images\Mjd-logo.png" class="Logo">
+        <h2>HARDWARE<br><span>HEROES</span></h2>
+        <nav>
+            <ul>
+                <li><a href="">HOME</a></li>
+            </ul>
+        </nav>
+        <img src="Images\Menu-button.png" class="menu-icon">
+    </div>
+    <div class="row">
+        <div class="column1">
+            <h2>WELCOME<br><span>TO THE</span></h2>
+            <h3>FUTURE</h3>
+            <p>Hello, my name is Michael John Diopenes, 
+            and I'm now enrolled in BSIT 1 at La Consiolacion College in Bacolod. 
+            I hope you enjoy my webpage.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
 
